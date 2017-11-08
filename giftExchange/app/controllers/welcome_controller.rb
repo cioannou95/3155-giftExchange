639f888b5a50f69c82e8ea464ff826a555946c02
Nikolaus Kreiling <nkreilin@uncc.edu>
2017-11-08 01:55:15 +0000

    Added routes to Welcome/newUser

--- giftExchange/app/controllers/welcome_controller.rb
+++ giftExchange/app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
   end
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  def create
+    render plain: params[:welcome].inspect
+  end
 end
