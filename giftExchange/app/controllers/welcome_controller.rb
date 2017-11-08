f37d223532cf05f0ffa687905f0dfbff87d3496c
Nikolaus Kreiling <nkreilin@uncc.edu>
2017-11-08 23:19:18 +0000

    Working on New User button

--- giftExchange/app/controllers/welcome_controller.rb
+++ giftExchange/app/controllers/welcome_controller.rb
@@ -1,19 +1,27 @@
 class WelcomeController < ApplicationController
   def index
   end
+
   
   def show
-    @welcome = Welcome.find(params[:id])
+    @user = Welcome.find(user_params)
   end
   
   def new
+    @user = Welcome.find(user_params)
   end
   
   def create
     #render plain: params[:welcome].inspect
-    @welcome = Welcome.new(params[:welcome])
+    @user = Welcome.new(user_params)
     
-    @welcome.save
-    redirect_to @welcome
+    @user.save
+    redirect_to @user
+    
+  end
+  
+private
+  def user_params
+    params.require(:user).permit(:fname, :lname)
   end
 end
--- giftExchange/config/routes.rb
+++ giftExchange/config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
   
+  resources :welcome
+  
   root 'welcome#index'
   
-  resources :welcome
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
