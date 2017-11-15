15b2849c771dd8319d60f71ed558763a55a15756
Nikolaus Kreiling <nkreilin@uncc.edu>
2017-11-15 21:11:04 +0000

    Added listing of users, validation for filling out new user form, error messages while filling out new user form

--- giftExchange/app/models/user.rb
+++ giftExchange/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :fname, presence: true, length: {minimum: 1}
+    validates :lname, presence: true, length: {minimum: 1}
+    validates :username, presence: true, length: {minimum: 1}
+    validates :password, presence: true, length: {minimum: 6}
+    validates :address, presence: true, length: {minimum: 1}
+    validates :wish_list, presence: true, length: {minimum: 5}
 end
