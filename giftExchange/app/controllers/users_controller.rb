227bd19c1542a34d2eb81469a10cbafa9e57f846
Nikolaus Kreiling <nkreilin@uncc.edu>
2017-11-15 01:58:54 +0000

    Can add users to database from users/new

--- giftExchange/app/controllers/users_controller.rb
+++ giftExchange/app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+    end
+    
     def new
     end
     
     def create
-        @user = User.new(params[user_params])
+        @user = User.new(user_params)
         
         @user.save
         redirect_to @user
