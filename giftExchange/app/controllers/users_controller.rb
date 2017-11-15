f665a42d450b139bd3830d2aa1d82180ad946c9c
Nikolaus Kreiling <nkreilin@uncc.edu>
2017-11-15 01:35:32 +0000

    Working on adding users to database

--- giftExchange/app/controllers/users_controller.rb
+++ giftExchange/app/controllers/users_controller.rb
@@ -3,6 +3,14 @@ class UsersController < ApplicationController
     end
     
     def create
-        render plain: params[:user].inspect
+        @user = User.new(params[user_params])
+        
+        @user.save
+        redirect_to @user
     end
 end
+
+private
+    def user_params
+        params.require(:user).permit(:fname, :lname, :username, :password, :email, :address, :wish_list)
+    end
