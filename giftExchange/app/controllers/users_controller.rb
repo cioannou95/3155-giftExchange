9ca12641e0c5b96ec219e86a1876c35a4c52b08f
Nikolaus Kreiling <nkreilin@uncc.edu>
2017-11-16 03:22:08 +0000

    Added update and delete functionality for Users

--- giftExchange/app/controllers/users_controller.rb
+++ giftExchange/app/controllers/users_controller.rb
@@ -21,6 +21,26 @@ class UsersController < ApplicationController
         end
     end
     
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        
+        redirect_to users_path
+    end
 end
 
 private
