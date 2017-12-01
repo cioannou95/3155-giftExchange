e4e2bd34ca1ff7852637d1c5b35f5ad470446bce
Dino Ioannou <cioannou@uncc.edu>
2017-12-01 18:07:17 +0000

    Made new branch for altering database, to incorporate google sign in functionality

--- giftExchange/config/routes.rb
+++ giftExchange/config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  get 'rooms/login'
 
-  get 'new/index'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure',  to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
   
   
-  resources :rooms 
+  resources :sessions, only: [:create, :destroy]
+  get 'welcome/index'
+  
+  
+  resources :rooms #do
+  #resources :comments
   
   #root 'rooms#login'
-  
+  #end
 
   resources :users
   
