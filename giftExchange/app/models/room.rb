e66c81d94b35f88968f42f091082159f3c02bf5c
Nikolaus Kreiling <nkreilin@uncc.edu>
2017-12-06 00:16:38 +0000

    Changing relationships to has_many

--- giftExchange/app/models/room.rb
+++ giftExchange/app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-   has_and_belongs_to_many :users
+   has_many :users
    validates :roomcode, presence: true, length: {minimum: 4}
    validates :roompassword, presence: true, length: {minimum: 4}
    
