0efd8b9a6e15b4ea59d0bc73f19cee10c1089115
Nikolaus Kreiling <nkreilin@uncc.edu>
2017-12-10 23:18:32 +0000

    Added destroy functionality for matches contoller

--- giftExchange/app/controllers/matches_controller.rb
+++ giftExchange/app/controllers/matches_controller.rb
@@ -5,6 +5,13 @@ class MatchesController < ApplicationController
         redirect_to room_path(@room)
     end
     
+    def destroy
+        @room = Room.find(params[:room_id])
+        @match = @room.matches.find(params[:id])
+        @match.destroy
+        redirect_to room_path(@room)
+    end
+    
     private
     def match_params
         params.require(:match).permit(:name)
