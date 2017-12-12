869264eb42f3893cde01c04662f5b8257795d6c5
Nikolaus Kreiling <nkreilin@uncc.edu>
2017-12-12 03:28:41 +0000

    Fixed routes for matches and sortNames

--- giftExchange/app/controllers/matches_controller.rb
+++ giftExchange/app/controllers/matches_controller.rb
@@ -12,6 +12,33 @@ class MatchesController < ApplicationController
         redirect_to room_path(@room)
     end
     
+    def sortNames
+    @room = Room.find(params[:id])
+    
+    value = 0
+    #counter to determine when algorithm is complete
+    
+    @names = ["nik", "joe", "roa", "dino", "alex", "kent", "tom", "taylor", "erin", "tessa"]
+    #this array needs to be populated from the database
+    
+    until value == @names.length
+      value = 0
+      @matchedNames = @names.shuffle
+      #shuffles names array into new array named matchedNames
+
+      @names.zip @matchedNames
+
+      @names.zip(@matchedNames).each do |names, matchedNames|
+        #print names, " ", matchedNames, "\n"
+        if names != matchedNames
+            value += 1
+            #adds one to value if names at given location don't match between arrays
+        end
+      end
+    end
+    
+  end
+    
     private
     def match_params
         params.require(:match).permit(:name)
